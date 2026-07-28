5e21850d003db0d230ca4659f2982004ce69fff7
VishalGowda23 <gowdavishal05@gmail.com>
2026-07-28 07:18:27 +0530

    Time: 1 ms (99.88%), Space: 43.2 MB (36.12%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,17 +1,27 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int[] freq = new int[26];
+        // int[] freq = new int[26];
+
+        // for(char c : s.toCharArray()){
+        //     freq[c-'a']++;
+        // }
+        // for(char c:t.toCharArray()){
+        //     freq[c-'a']--;
+
+        //     if(freq[c-'a'] < 0){
+        //         return c;
+        //     }
+        // }
+        // return ' ';
+
+        char ans = 0;
 
         for(char c : s.toCharArray()){
-            freq[c-'a']++;
+            ans ^= c;
         }
-        for(char c:t.toCharArray()){
-            freq[c-'a']--;
-
-            if(freq[c-'a'] < 0){
-                return c;
-            }
+        for(char c : t.toCharArray()){
+            ans ^= c;
         }
-        return ' ';
+        return ans;
     }
 }
