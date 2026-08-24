a0fab0cfa0cbbd4df406a9f630c1e11b2132ed3a
VishalGowda23 <gowdavishal05@gmail.com>
2026-08-24 21:05:25 +0530

    Time: 19 ms (83.22%), Space: 54.3 MB (43.29%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,18 +1,40 @@
 class Solution {
     public int equalPairs(int[][] grid) {
+        // int count = 0;
+        // for(int i = 0 ; i < grid.length ; i++){
+        //     for(int j = 0 ; j < grid.length ; j++){
+        //         boolean equal = true;
+        //         for(int k = 0 ; k < grid.length ;k++){
+        //             if(grid[i][k] != grid[k][j]){
+        //                 equal = false;
+        //                 break;
+        //             }
+        //         }
+        //         if(equal){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+        HashMap<List<Integer>, Integer>map = new HashMap<>();
         int count = 0;
+
         for(int i = 0 ; i < grid.length ; i++){
+            List<Integer> row = new ArrayList<>();
             for(int j = 0 ; j < grid.length ; j++){
-                boolean equal = true;
-                for(int k = 0 ; k < grid.length ;k++){
-                    if(grid[i][k] != grid[k][j]){
-                        equal = false;
-                        break;
-                    }
-                }
-                if(equal){
-                    count++;
-                }
+                row.add(grid[i][j]);
+            }
+            map.put(row , map.getOrDefault(row , 0) + 1);
+        }
+
+        for(int i = 0 ; i < grid.length ; i++){
+            List<Integer> col = new ArrayList<>();
+            for(int j = 0 ; j < grid.length ; j++){
+                col.add(grid[j][i]);
+            }
+
+            if(map.containsKey(col)){
+                count += map.get(col);
             }
         }
         return count;
