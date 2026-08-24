99d4dcf6299e367a44e225f1791541e4fec0a4c5
VishalGowda23 <gowdavishal05@gmail.com>
2026-08-24 20:10:58 +0530

    Time: 13 ms (44.37%), Space: 47.9 MB (53.59%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -6,23 +6,46 @@ class Solution {
             return false;
         }
 
-        for(char ch : word1.toCharArray()){
-            map1.put(ch , map1.getOrDefault(ch,0)+1);
+        // for(char ch : word1.toCharArray()){
+        //     map1.put(ch , map1.getOrDefault(ch,0)+1);
+        // }
+        // for(char ch : word2.toCharArray()){
+        //     map2.put(ch , map2.getOrDefault(ch,0)+1);
+        // }
+
+        // if(!map1.keySet().equals(map2.keySet())){
+        //     return false;
+        // }
+
+        // ArrayList<Integer> freq1 = new ArrayList<>(map1.values());
+        // ArrayList<Integer> freq2 = new ArrayList<>(map2.values());
+
+        // Collections.sort(freq1);
+        // Collections.sort(freq2);
+
+        // return freq1.equals(freq2);
+
+
+        int [] count1 = new int[26];
+        int [] count2 = new int[26];
+        
+
+        for(char c : word1.toCharArray()){
+            count1[c - 'a']++;
         }
-        for(char ch : word2.toCharArray()){
-            map2.put(ch , map2.getOrDefault(ch,0)+1);
+        for(char c : word2.toCharArray()){
+            count2[c - 'a']++;
         }
 
-        if(!map1.keySet().equals(map2.keySet())){
-            return false;
+        for(int i = 0 ; i < 26 ; i++ ){
+            if(count1[i]== 0 && count2[i]!=0 || count1[i]!=0 && count2[i]==0){
+                return false;
+            }
         }
 
-        ArrayList<Integer> freq1 = new ArrayList<>(map1.values());
-        ArrayList<Integer> freq2 = new ArrayList<>(map2.values());
+        Arrays.sort(count1);
+        Arrays.sort(count2);
 
-        Collections.sort(freq1);
-        Collections.sort(freq2);
-
-        return freq1.equals(freq2);
+        return Arrays.equals(count1,count2);
     }
 }
