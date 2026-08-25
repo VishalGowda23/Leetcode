993a34510dc5a8bb1efed0938e9e1bcdd20e2798
VishalGowda23 <gowdavishal05@gmail.com>
2026-08-25 21:10:12 +0530

    Time: 3 ms (20.57%), Space: 43.3 MB (20.28%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,31 +1,56 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        StringBuilder resultS = new StringBuilder();
-        char[] j = s.toCharArray();
-        for(int i =0 ; i < j.length ; i++){
-            if(j[i] == '#'){
-                if(resultS.length() > 0){
+        // StringBuilder resultS = new StringBuilder();
+        // char[] j = s.toCharArray();
+        // for(int i =0 ; i < j.length ; i++){
+        //     if(j[i] == '#'){
+        //         if(resultS.length() > 0){
 
-                resultS.deleteCharAt(resultS.length() - 1);
+        //         resultS.deleteCharAt(resultS.length() - 1);
+        //         }
+        //     }
+        //     else{
+        //         resultS.append(j[i]);
+        //     }
+        // }
+        // StringBuilder resultT = new StringBuilder();
+        // char[] k = t.toCharArray();
+        // for(int i =0 ; i < k.length ; i++){
+        //     if(k[i] == '#'){
+        //         if(resultT.length() > 0){
+
+        //         resultT.deleteCharAt(resultT.length() - 1);
+        //         }
+        //     }
+        //     else{
+        //         resultT.append(k[i]);
+        //     }
+        // }
+        // return resultS.toString().equals(resultT.toString());
+
+        Stack<Character> stackS = new Stack<>();
+        Stack<Character> stackT = new Stack<>();
+
+        for(int i = 0 ; i < s.length() ;i++){
+            if(s.charAt(i) == '#'){
+                if(!stackS.isEmpty()){
+                    stackS.pop();
                 }
             }
             else{
-                resultS.append(j[i]);
+                stackS.push(s.charAt(i));
             }
         }
-        StringBuilder resultT = new StringBuilder();
-        char[] k = t.toCharArray();
-        for(int i =0 ; i < k.length ; i++){
-            if(k[i] == '#'){
-                if(resultT.length() > 0){
-
-                resultT.deleteCharAt(resultT.length() - 1);
+        for(int i = 0 ; i < t.length() ;i++){
+            if(t.charAt(i) == '#'){
+                if(!stackT.isEmpty()){
+                    stackT.pop();
                 }
             }
             else{
-                resultT.append(k[i]);
+                stackT.push(t.charAt(i));
             }
         }
-        return resultS.toString().equals(resultT.toString());
+        return stackS.equals(stackT);
     }
 }
