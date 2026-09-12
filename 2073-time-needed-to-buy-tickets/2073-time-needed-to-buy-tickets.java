214dd838566c81f74c3b9b59d8a9a97923156a50
VishalGowda23 <gowdavishal05@gmail.com>
2026-09-12 20:05:30 +0530

    Time: 2 ms (56.23%), Space: 42.9 MB (85.9%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int count = 0;
+        int i = 0 ;
+        while(tickets[k] != 0){
+
+            if(tickets[i] > 0){
+                tickets[i]--;
+                count++;
+            }
+
+            i++;
+            if(i ==  tickets.length){
+                i = 0 ;
+            }
+        }
+        return count;
+    }
+}
